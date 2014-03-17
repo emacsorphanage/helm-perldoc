8012b1929eac00bb5d003a8794eb534cc4e499fa
Syohei YOSHIDA <syohex@gmail.com>
2014-03-17 23:53:42 +0900

    Bump up version and update Changes

--- helm-perldoc.el
+++ helm-perldoc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-perldoc
-;; Version: 0.03
+;; Version: 0.04
 ;; Package-Requires: ((helm "1.0") (deferred "0.3.1") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
