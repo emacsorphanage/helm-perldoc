3fabc84fbe640d392af1186fd10eebdcec32249e
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-03 11:39:24 +0300

    Add helm-perldoc-at-point

--- helm-perldoc.el
+++ helm-perldoc.el
@@ -359,13 +359,18 @@
          (kill-buffer))))))
 
 ;;;###autoload
-(defun helm-perldoc ()
+(defun helm-perldoc (&optional input)
   (interactive)
   (helm-perldoc:check-buffer)
   (helm :sources '(helm-perldoc:imported-source
                    helm-perldoc:superclass-source
                    helm-perldoc:other-source)
+        :input input
         :buffer "*helm-perldoc*"))
+
+(defun helm-perldoc-at-point ()
+  (interactive)
+  (helm-perldoc (thing-at-point 'symbol)))
 
 (provide 'helm-perldoc)
 
